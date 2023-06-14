14d26b528e088e96161d917fce4cc5935be6463d
huntleyh <52048389+huntleyh@users.noreply.github.com>
2023-06-14 14:39:32 -0400

    update border color to green

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -100,14 +100,7 @@ export default class App extends Vue {
 #bottom,
 #left,
 #right {
-  background: linear-gradient(
-    45deg,
-    #f09433 0%,
-    #e6683c 25%,
-    #dc2743 50%,
-    #cc2366 75%,
-    #bc1888 100%
-  );
+  background-color: green;
   position: fixed;
 }
 #left,
